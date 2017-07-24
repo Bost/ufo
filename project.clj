9558c5394190e048ef797136b8f7726b05f65269
Bost <thebost@gmail.com>
2017-07-24 19:20:03 +0200

    mysql/mysql-connector-java "8.0.7"

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
 
    [org.clojure/java.jdbc "0.6.1"]
    [com.mchange/c3p0 "0.9.5.2"] ; db connection pooling
-   [mysql/mysql-connector-java "6.0.6"]
+   [mysql/mysql-connector-java "8.0.7"]
 
    ;; 0.9.0 requires new db2jcc4.jar and {:classname ... :jdbc-url ...}
    [clj-dbcp "0.8.2"] ; JDBC connections pools
