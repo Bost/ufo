1a37ea159ebe976b053928ceeab3a8c26b82d425
Bost <thebost@gmail.com>
2018-07-02 18:52:29 +0200

    Cleanup

--- src/cljs/ufo/views.cljs
+++ src/cljs/ufo/views.cljs
@@ -4,10 +4,9 @@
 (enable-console-print!)
 
 (defn thead [cols]
-  (fn []
-    [:tr
-     (map-indexed (fn [i v]
-                    [:th {:key i} (str v)]) cols)]))
+  [:tr
+   (map-indexed (fn [i v]
+                  [:th {:key i} (str v)]) cols)])
 
 (defn td [i v loading?]
   [:td (conj {:key (str "tr-" i "-" v)}
@@ -19,48 +18,35 @@
                            (re-frame/dispatch [:toggle v]))})
    v])
 
-(defn tbody []
-  (fn []
-    (let [loading? (re-frame/subscribe [:loading?])
-          emps (re-frame/subscribe [:emps])]
-      [:tbody
-       (doall
-        (map-indexed
-         (fn [i [id-val hm]]
-           [:tr {:key (str "tr-" i)}
-            (let [id (name id-val)
-                  salary-val (:salary hm)
-                  salary (if salary-val salary-val ;; auto-onclick
-                             (re-frame/dispatch [:id id]))
-                  abbrev-val (:abbrev hm)
-                  abbrev (if abbrev-val abbrev-val ;; auto-onclick
-                             (re-frame/dispatch [:id id]))]
-              (doall
-               (map #(td i % @loading?)
-                    (remove nil? [id salary abbrev]))))])
-         @emps))])))
-
 (defn table [id]
   (let [table-def (re-frame/subscribe [:tables])]
-    (fn []
-      [:div
-       [:div (->> @table-def id :name)]
-       [:table
-        [:thead [thead (->> @table-def id :cols)]]
-        [tbody]
-        ]])))
-
-(defn loading-throbber
-  []
-  (let [loading? (re-frame/subscribe [:loading?])]
-    (when @loading?
-      (do
-        #_(println "Loading...")
-        [:div.loading
-         [:div.three-quarters-loader "Loading..."]]))))
+    [:div
+     [:div (->> @table-def id :name)]
+     [:table
+      [:thead (thead (->> @table-def id :cols))]
+      [:tbody
+       (let [loading? (re-frame/subscribe [:loading?])]
+         (doall
+          (map-indexed
+           (fn [i [id-val hm]]
+             [:tr {:key (str "tr-" i)}
+              (let [id (name id-val)
+                    salary-val (:salary hm)
+                    salary (if salary-val salary-val ;; auto-onclick
+                               (re-frame/dispatch [:id id]))
+                    abbrev-val (:abbrev hm)
+                    abbrev (if abbrev-val abbrev-val ;; auto-onclick
+                               (re-frame/dispatch [:id id]))]
+                (doall
+                 (map #(td i % @loading?)
+                      (remove nil? [id salary abbrev]))))])
+           @(re-frame/subscribe [:emps]))))]
+      ]]))
 
 (defn main-panel []
   (fn []
     [:div
-     [loading-throbber]
-     [table :salaries]]))
+     (when @(re-frame/subscribe [:loading?])
+       [:div.loading
+        [:div.three-quarters-loader "Loading..."]])
+     (table :salaries)]))
